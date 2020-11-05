0502cd50e3211c8a5b8ad131667acbc9a373f0d5
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-11-05 15:00:55 +0530

    Added: UC1 functionality added to the code successfully on branch uc1

--- employeeWage.js
+++ employeeWage.js
@@ -1,1 +1,14 @@
-console.log("Hello! Welcome to employee wage calculator.");
+console.log("---------Hello! Welcome to employee wage calculator---------");
+console.log("\n");
+
+//UC1
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+
+    if (empCheck == IS_ABSENT) {
+        console.log("Employee is absent. Exiting the program!!!");
+    } else {
+        console.log("Employee is present");
+    }
+}
