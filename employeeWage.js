d6185b2090159bd4955c534e34aba91723816e39
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-11-05 15:17:00 +0530

    Added: UC3 functionality successfully executed and added to the code at branch uc3

--- employeeWage.js
+++ employeeWage.js
@@ -16,28 +16,29 @@ console.log("\n");
 
 console.log("\n");
 
-//UC2
+//UC2 refactored to UC3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let employeeHours = 0;
+
 let employeeCheck = Math.floor(Math.random() * 10) % 3;
 
-switch (employeeCheck) {
-    case IS_PART_TIME:
-        employeeHours = PART_TIME_HOURS;
-        break;
+function getWorkingHours(employeeCheckIndex) {
+    switch (employeeCheckIndex) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
 
-    case IS_FULL_TIME:
-        employeeHours = FULL_TIME_HOURS;
-        break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
 
-    default:
-        break;
+        default:
+            return 0;
+    }
 }
 
+let employeeHours = getWorkingHours(employeeCheck);
 let employeeWage = employeeHours * 20;
 console.log("Employee wage would be : " + employeeWage);
