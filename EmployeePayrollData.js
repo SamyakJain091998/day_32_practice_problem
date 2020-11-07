ccadcdd8cfc246d75a448ff758cd6e9223a28dec
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-11-07 23:22:50 +0530

    Added: UC15 functionalities added to the code at branhc uc15

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,8 +1,4 @@
 class EmployeePayrollData{
-    id;
-    salary;
-    gender;
-    startDate;
 
     constructor(...params){
         this.id = params[0];
@@ -15,10 +11,51 @@ class EmployeePayrollData{
     get name() {
         return this._name;
     }
+
     set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if(nameRegex.test(name)) this._name = name;
+        if(nameRegex.test(name)){
+            this._name = name;
+        } 
         else throw 'Name is incorrect';
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id){
+        let idRegex = RegExp('^[1-9]{1,}[0-9]{0,}$');
+        if(idRegex.test(id)){
+            this._id = id;
+        }
+        else throw 'Id is incorrect';
+    }
+    
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary){
+        let salaryRegex = RegExp('^[1-9]{1}[0-9]{0,}$');
+        if(salaryRegex.test(salary)){
+            this._salary = salary;
+        }
+        else throw 'Salary is incorrect';
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        if (this._gender === undefined) {
+            this._gender = "m";
+        } else {
+            let genderRegex = RegExp('^[a-zA-Z]{1}$');
+            if (genderRegex.test(gender)) this._gender = gender;
+            else throw 'Gender is incorrect';
+        }
     }
 
     toString() {
@@ -36,9 +73,32 @@ console.log(employeePayrollData.toString());
 employeePayrollData = new EmployeePayrollData(2, "Jeff", 40000, "M", new Date());
 console.log(employeePayrollData.toString());
 
-try{
-    employeePayrollData.name = "john";
+try {
+    employeePayrollData.name = "John";
     console.log(employeePayrollData.toString());
-}catch (e){
+} catch (e) {
     console.error(e);
 }
+
+try {
+    employeePayrollData.id = 10;
+    console.log(employeePayrollData.toString());
+} catch (e) {
+    console.error(e);
+}
+
+try {
+    employeePayrollData.salary = 60000;
+    console.log(employeePayrollData.toString());
+} catch (e) {
+    console.error(e);
+}
+
+try {
+    employeePayrollData.gender = "f";
+    console.log(employeePayrollData.toString());
+} catch (e) {
+    console.error(e);
+}
+
+
