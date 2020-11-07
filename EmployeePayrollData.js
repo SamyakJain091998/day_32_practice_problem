9464584b9685a39a253b731ce92b722efec2ef1c
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-11-07 18:49:33 +0530

    Added: UC12 functionality added to the code at branch uc12

---
+++ EmployeePayrollData.js
@@ -0,0 +1,31 @@
+class EmployeePayrollData{
+    id;
+    salary;
+    gender;
+    startDate;
+
+    constructor(id, name, salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        console.log("setter method");
+        this._name = name;
+    }
+
+    toString() {
+        return "id: " + this.id + ", name: " + this.name + ", salary: " + this.salary;
+    }
+}
+
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
+console.log(employeePayrollData.toString());
+
+employeePayrollData.id = 0;
+employeePayrollData.name = "Jeff";
+console.log(employeePayrollData.toString());
